8d84a973512ef6c0b8461a2abd6d764da58ae8a9
Dan Duvall <dduvall@wikimedia.org>
2024-03-08 14:41:25 -0800

    Added apt/main.go

---
+++ apt/main.go
@@ -0,0 +1,23 @@
+package main
+
+type Apt struct{}
+
+var aptInstall = []string{"apt-get", "install", "-y"}
+
+func (apt *Apt) Install(packages []string) WithContainerFunc {
+	return func(ctr *Container) {
+		return ctr.
+			WithMountedCache(
+				"/var/lib/apt",
+				dag.CacheVolume("/var/lib/apt"),
+				ContainerWithMountedCacheOpts{Sharing: Locked},
+			).
+			WithMountedCache(
+				"/var/cache/apt",
+				dag.CacheVolume("/var/cache/apt"),
+				ContainerWithMountedCacheOpts{Sharing: Locked},
+			).
+			WithEnvVariable("DEBIAN_FRONTEND", "noninteractive").
+			WithExec(append(aptInstall, packages...))
+	}
+}
