fb4b50df214c8c2d23c27cdc40dfd95f79187920
Dan Duvall <dduvall@wikimedia.org>
2024-03-11 10:57:25 -0700

    Factor out use of WithContainerFunc which is not supported
    Only return types that can be serialized are supported.

--- apt/main.go
+++ apt/main.go
@@ -4,20 +4,18 @@ type Apt struct{}
 
 var aptInstall = []string{"apt-get", "install", "-y"}
 
-func (apt *Apt) Install(packages []string) WithContainerFunc {
-	return WithContainerFunc(func(ctr *Container) {
-		return ctr.
-			WithMountedCache(
-				"/var/lib/apt",
-				dag.CacheVolume("/var/lib/apt"),
-				ContainerWithMountedCacheOpts{Sharing: Locked},
-			).
-			WithMountedCache(
-				"/var/cache/apt",
-				dag.CacheVolume("/var/cache/apt"),
-				ContainerWithMountedCacheOpts{Sharing: Locked},
-			).
-			WithEnvVariable("DEBIAN_FRONTEND", "noninteractive").
-			WithExec(append(aptInstall, packages...))
-	})
+func (apt *Apt) Install(ctr *Container, packages []string) *Container {
+	return ctr.
+		WithMountedCache(
+			"/var/lib/apt",
+			dag.CacheVolume("/var/lib/apt"),
+			ContainerWithMountedCacheOpts{Sharing: Locked},
+		).
+		WithMountedCache(
+			"/var/cache/apt",
+			dag.CacheVolume("/var/cache/apt"),
+			ContainerWithMountedCacheOpts{Sharing: Locked},
+		).
+		WithEnvVariable("DEBIAN_FRONTEND", "noninteractive").
+		WithExec(append(aptInstall, packages...))
 }
